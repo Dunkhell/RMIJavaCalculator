d83635608de976c1bebefaf8a4f6df98ff74be2e
Dunell <ciemnoki12@gmail.com>
2023-05-31 21:47:58 +0200

    Can't divide by 0

--- src/main/java/org/uwm/sys/rozpro/calculator/server/service/CalculatorService.java
+++ src/main/java/org/uwm/sys/rozpro/calculator/server/service/CalculatorService.java
@@ -3,6 +3,8 @@ package org.uwm.sys.rozpro.calculator.server.service;
 import lombok.RequiredArgsConstructor;
 import org.uwm.sys.rozpro.calculator.server.model.CalculatorInterface;
 
+import java.rmi.RemoteException;
+
 
 @RequiredArgsConstructor
 public class CalculatorService implements CalculatorInterface {
@@ -19,7 +21,10 @@ public class CalculatorService implements CalculatorInterface {
         return num1 * num2;
     }
 
-    public double divide(double num1, double num2) {
+    public double divide(double num1, double num2) throws RemoteException {
+        if (num2 == 0) {
+            throw new RemoteException("Cannot divide by zero!");
+        }
         return num1 / num2;
     }
 }
